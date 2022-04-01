7b99a343f8b293cb7cb2043c3d6185e4aff54170
kzw200015 <kzw200015@gmail.com>
2022-04-01 12:26:57 +0800

    Add CI

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    title: '猫雷NyaRu',
-    description: '一起大喊，猫雷最强！',
+    title: "猫雷NyaRu",
+    description: "猫雷最强！",
     themeConfig: {
         nav: [
-            { text: '个人简介', link: '/introduction' },
-            { text: '精选切片', link: '/splice' },
-            { text: '猫雷按钮', link: '/button' },
-            { text: '关于', link: '/about' }
+            { text: "个人简介", link: "/introduction" },
+            { text: "精选切片", link: "/splice" },
+            { text: "猫雷按钮", link: "/button" },
+            { text: "关于", link: "/about" },
+            { text: "Github", link: "https://github.com/kzw200015/nyaru" }
         ]
     }
 }
